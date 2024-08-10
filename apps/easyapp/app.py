25a7186c903ed9a2623a7e484b0b3ae171115cb7
lochxie <sylove0703@gamil.com>
2024-08-10 10:49:50 +0900

    240810 commit

--- apps/easyapp/app.py
+++ apps/easyapp/app.py
@@ -1,6 +1,19 @@
-from flask import Flask, render_template, url_for, request, redirect
+import os
+from flask_mail import Mail, Message
+from flask import Flask, render_template, url_for, request, redirect, flash
+from email_validator import validate_email, EmailNotValidError
 
 app = Flask(__name__)
+
+app.config["SECRET_KEY"] = b'\xb1\xa5SU\xfc\x16\xa7p\xabj\xe1\xbfr\x94h\xdd'
+
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = 'jojju486@gmail.com'
+app.config['MAIL_PASSWORD'] = 'stkm cgck saeq hlju'
+
+mail = Mail(app)
 
 @app.route("/")
 def index():
@@ -14,6 +27,12 @@ def hello(name):
 def show_name(name):
     return render_template("index.html", name=name)
 
+def send_email(to, subject, template, **kwargs):    
+    msg = Message(subject, sender='jojju486@gmail.com', recipients=[to])
+    msg.body = render_template(template + ".txt", **kwargs)
+    msg.html = render_template(template + ".html", **kwargs)
+    mail.send(msg)
+
 @app.route('/contact')
 def contact():
     return render_template('contact.html')
@@ -24,5 +43,37 @@ def contact_complete():
         username = request.form["username"]
         email = request.form["email"]
         description = request.form["description"]
+
+        is_valid = True
+
+        if not username:
+            flash("사용자명은 필수입니다.")
+            is_valid = False
+
+        if not email:
+            flash("메일주소는 필수입니다.")
+            is_valid = False
+        try:
+            validate_email(email)
+        except EmailNotValidError:
+            flash("메일주소의 형식으로 입력하세요.")
+            is_valid = False
+
+        if not description:
+            flash("문의 내용은 필수입니다.")
+            is_valid = False
+        
+        if not is_valid:
+            return redirect(url_for("contact"))
+        
+        send_email(
+            email,
+            "문의 감사합니다.",
+            "contact_mail",
+            username = username,
+            description = description
+        )
+        flash("문의 내용은 메일로 송신되었습니다. 문의해 주셔서 감사합니다.")
+
         return redirect(url_for('contact_complete'))
     return render_template("contact_complete.html")
